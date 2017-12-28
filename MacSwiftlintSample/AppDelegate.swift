1003d0283c4184dec6321b10c17adff1a1e34649
中野 雅章 <tzr250rs_94s@yahoo.co.jp>
2017-12-29 00:03:33 +0900

    プロジェクト追加

---
+++ MacSwiftlintSample/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacSwiftlintSample
+//
+//  Created by 電気なまず on 2017/12/29.
+//  Copyright © 2017年 Masanori Nakano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
